dbccffa0ab6c047c2d8822be908ff111f501736b
Roxanoel <roxane.noel92@gmail.com>
2022-08-04 15:08:11 +0100

    Copied tests from fibonacci and adapted them.

--- fibsRec.test.js
+++ fibsRec.test.js
@@ -1,1 +1,18 @@
 const fibsRec = require('./fibsRec');
+
+test('Passing in 1 returns [0]', () => {
+    expect(fibsRec(1)).toStrictEqual([0]);
+});
+test('Returns correct fibonacci sequence', () => {
+    expect(fibsRec(8)).toStrictEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+    expect(fibsRec(5)).toStrictEqual([0, 1, 1, 2, 3]);
+});
+test('Returns "Invalid parameter" if non-number is passed in', () => {
+    expect(fibRecs('a')).toBe("Invalid parameter");
+});
+test('Returns "Invalid parameter" if negative number is passed in', () => {
+    expect(fibRecs(-8)).toBe("Invalid parameter");
+});
+test('Returns "Invalid parameter" if 0 is passed in', () => {
+    expect(fibRecs(0)).toBe("Invalid parameter");
+});
