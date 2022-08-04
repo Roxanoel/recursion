dbde0a416d60161d11f1d0d94340b95c9fdce231
Roxanoel <roxane.noel92@gmail.com>
2022-08-04 14:03:59 +0100

    Wrote tests for fibonacci(1) and fibonacci(0).

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -1,5 +1,10 @@
+const { expect } = require('expect');
+const { test } = require('picomatch');
 const fibonacci = require('./fibonacci');
 
+test('Passing in 1 returns [0]', () => {
+    expect(fibonacci(1)).toStrictEqual([0]);
+});
 test('Returns correct fibonacci sequence', () => {
     expect(fibonacci(8)).toStrictEqual([0, 1, 1, 2, 3, 5, 8, 13]);
     expect(fibonacci(5)).toStrictEqual([0, 1, 1, 2, 3]);
@@ -9,4 +14,7 @@ test('Returns "Invalid parameter" if non-number is passed in', () => {
 });
 test('Returns "Invalid parameter" if negative number is passed in', () => {
     expect(fibonacci(-8).toBe("Invalid parameter"));
+});
+test('Returns "Invalid parameter" if 0 is passed in', () => {
+    expect(fibonacci(0).toBe("Invalid parameter"));
 });
