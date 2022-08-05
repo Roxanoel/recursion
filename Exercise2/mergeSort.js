dcf10ad38740d06fa21ce486b8401c9821fdd00b
Roxanoel <roxane.noel92@gmail.com>
2022-08-05 10:21:13 +0100

    Passed tests for basic cases. Even array, even array without duplicates.

--- Exercise2/mergeSort.js
+++ Exercise2/mergeSort.js
@@ -5,6 +5,40 @@ function mergeSort(array) {
     if(array.length === 1) return array;
 
     // If array is bigger than 1, cut it in two
+    const pivot = Math.floor(array.length / 2);
+    console.log(pivot);
+
+    let left = array.slice(0, pivot);
+    let right = array.slice(pivot, array.length);
+
+    // Step 1: Sort the left side array
+    console.log(left);
+    left = mergeSort(left);
+    console.log(left);
+    // Step 2: Sort the right side array
+    console.log(right);
+    right = mergeSort(right);
+    console.log(right);
+    // Step 3: Merge
+    const merged = merge(left, right);
+    return merged;
+}      
+
+function merge(left, right) {
+    const merged = [];
+
+    for (let i = 0; i < right.length; i ++) {   // Because I use floor, if one side is bigger, it will be the right side. 
+        if (left[i] <= right[i]) {
+            merged.push(left[i]);
+            merged.push(right[i]);
+        } else {
+            merged.push(right[i]);
+            merged.push(left[i]);
+        }
+    }
+
+    return merged;
 }
+
 
 module.exports = mergeSort;
